9ca899859b2beddbaf74736570d7f6a2e19dbd57
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 03:15:21 +0700

    added test case emailIsnotEmpty in class ValidateEmailSuccessTest

--- app/src/test/java/cc/somkiat/basicunittesting/ValidateEmailSuccessTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/ValidateEmailSuccessTest.java
@@ -1,4 +1,17 @@
 package cc.somkiat.basicunittesting;
 
+import org.junit.Test;
+
+import cc.somkiat.basicunittesting.validator.EmailValidator;
+
+import static junit.framework.Assert.assertFalse;
+
 public class ValidateEmailSuccessTest {
+
+    @Test
+    public void emailIsNotEmpty() {
+        EmailValidator validator = new EmailValidator();
+        boolean result = validator.isEmailEmpty("taweewong@gmail.com");
+        assertFalse("Email is empty", result);
+    }
 }
