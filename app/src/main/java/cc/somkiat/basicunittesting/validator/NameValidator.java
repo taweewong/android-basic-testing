e1027850751bbdfe43a3f558b37461bd341ae914
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 02:52:32 +0700

    added test case nameIsNotEmpty in class ValidateNameSuccessTest

--- app/src/main/java/cc/somkiat/basicunittesting/validator/NameValidator.java
+++ app/src/main/java/cc/somkiat/basicunittesting/validator/NameValidator.java
@@ -1,4 +1,8 @@
 package cc.somkiat.basicunittesting.validator;
 
 public class NameValidator {
+
+    public boolean isNameEmpty(String name) {
+        return name.isEmpty();
+    }
 }
--- app/src/test/java/cc/somkiat/basicunittesting/ValidateNameSuccessTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/ValidateNameSuccessTest.java
@@ -1,4 +1,17 @@
 package cc.somkiat.basicunittesting;
 
+import org.junit.Test;
+
+import cc.somkiat.basicunittesting.validator.NameValidator;
+
+import static junit.framework.Assert.assertFalse;
+
 public class ValidateNameSuccessTest {
+
+    @Test
+    public void nameIsNotEmpty() {
+        NameValidator validator = new NameValidator();
+        boolean result = validator.isNameEmpty("Taweewong");
+        assertFalse("Name is empty", result);
+    }
 }
