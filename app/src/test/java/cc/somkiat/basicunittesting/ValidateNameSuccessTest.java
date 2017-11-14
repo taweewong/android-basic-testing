dd6edcf292bcb0c955051f4c1516d591ff068c0c
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 03:08:15 +0700

    added test case nameHasOnlyAlphabet in class ValidateNameSuccessTest

--- app/src/test/java/cc/somkiat/basicunittesting/ValidateNameSuccessTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/ValidateNameSuccessTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import cc.somkiat.basicunittesting.validator.NameValidator;
 
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 public class ValidateNameSuccessTest {
 
@@ -13,5 +14,12 @@ public class ValidateNameSuccessTest {
         NameValidator validator = new NameValidator();
         boolean result = validator.isNameEmpty("Taweewong");
         assertFalse("Name is empty", result);
+    }
+
+    @Test
+    public void nameHasOnlyAlphabet() {
+        NameValidator validator = new NameValidator();
+        boolean result = validator.isNameHasOnlyAlphabet("Tocharoen");
+        assertTrue("Name is not has only alphabet", result);
     }
 }
