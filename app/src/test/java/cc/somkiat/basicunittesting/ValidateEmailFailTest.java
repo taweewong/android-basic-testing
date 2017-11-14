8d30e1f1bb3bb0ebf89bd2cacbd2533372f7adf2
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 03:15:57 +0700

    added test case emailIsEmpty in class ValidateEmailFailTest

--- app/src/test/java/cc/somkiat/basicunittesting/ValidateEmailFailTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/ValidateEmailFailTest.java
@@ -1,4 +1,17 @@
 package cc.somkiat.basicunittesting;
 
+import org.junit.Test;
+
+import cc.somkiat.basicunittesting.validator.EmailValidator;
+
+import static junit.framework.Assert.assertTrue;
+
 public class ValidateEmailFailTest {
+
+    @Test
+    public void emailIsEmpty() {
+        EmailValidator validator = new EmailValidator();
+        boolean result = validator.isEmailEmpty("");
+        assertTrue("Email is not empty", result);
+    }
 }
