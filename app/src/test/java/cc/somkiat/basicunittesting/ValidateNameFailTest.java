3cd90bdc38366e877544ecb20d4d4460af399d3e
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 02:54:01 +0700

    added test case nameIsEmpty in class ValidateNameFailTest

--- app/src/test/java/cc/somkiat/basicunittesting/ValidateNameFailTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/ValidateNameFailTest.java
@@ -1,4 +1,17 @@
 package cc.somkiat.basicunittesting;
 
+import org.junit.Test;
+
+import cc.somkiat.basicunittesting.validator.NameValidator;
+
+import static junit.framework.Assert.assertTrue;
+
 public class ValidateNameFailTest {
+
+    @Test
+    public void nameIsEmpty() {
+        NameValidator validator = new NameValidator();
+        boolean result = validator.isNameEmpty("");
+        assertTrue("Name is not empty", result);
+    }
 }
