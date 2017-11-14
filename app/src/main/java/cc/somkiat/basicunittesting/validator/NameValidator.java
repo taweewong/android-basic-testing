b1838b40817062fb1c6629126e475d92d87d20a3
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 03:07:53 +0700

    added method isNameHasOnlyAlphabet in class NameValidator

--- app/src/main/java/cc/somkiat/basicunittesting/validator/NameValidator.java
+++ app/src/main/java/cc/somkiat/basicunittesting/validator/NameValidator.java
@@ -1,8 +1,14 @@
 package cc.somkiat.basicunittesting.validator;
 
+import java.util.regex.Pattern;
+
 public class NameValidator {
 
     public boolean isNameEmpty(String name) {
         return name.isEmpty();
+    }
+
+    public boolean isNameHasOnlyAlphabet(String name) {
+        return Pattern.matches("^[a-zA-Z]+$", name);
     }
 }
