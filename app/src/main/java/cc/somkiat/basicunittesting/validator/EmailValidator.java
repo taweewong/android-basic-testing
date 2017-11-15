915da4b10a679656dc6c07e6327a783580e3a403
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 13:02:15 +0700

    added method isEmailPatternCorrect in class EmailValidator

--- app/src/main/java/cc/somkiat/basicunittesting/validator/EmailValidator.java
+++ app/src/main/java/cc/somkiat/basicunittesting/validator/EmailValidator.java
@@ -1,8 +1,14 @@
 package cc.somkiat.basicunittesting.validator;
 
+import java.util.regex.Pattern;
+
 public class EmailValidator {
 
     public boolean isEmailEmpty(String email) {
         return email.isEmpty();
+    }
+
+    public boolean isEmailPatternCorrect(String email) {
+        return Pattern.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}", email);
     }
 }
