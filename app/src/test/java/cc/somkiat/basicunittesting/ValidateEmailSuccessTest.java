d51edc3597961e513514e08282d844629ca9ea16
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 13:02:39 +0700

    added test case emailPatternIsCorrect in class ValidateEmailSuccessTest

--- app/src/test/java/cc/somkiat/basicunittesting/ValidateEmailSuccessTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/ValidateEmailSuccessTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import cc.somkiat.basicunittesting.validator.EmailValidator;
 
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 public class ValidateEmailSuccessTest {
 
@@ -13,5 +14,12 @@ public class ValidateEmailSuccessTest {
         EmailValidator validator = new EmailValidator();
         boolean result = validator.isEmailEmpty("taweewong@gmail.com");
         assertFalse("Email is empty", result);
+    }
+
+    @Test
+    public void emailPatternIsCorrect() {
+        EmailValidator validator = new EmailValidator();
+        boolean result = validator.isEmailPatternCorrect("taweewong.t@gmail.com");
+        assertTrue("Email's pattern is incorrect", result);
     }
 }
