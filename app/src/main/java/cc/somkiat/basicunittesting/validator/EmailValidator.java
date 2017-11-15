b597dd0419b387c990cb48b9501184960d6681b9
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 14:40:23 +0700

    created method validate and made all method throw ValidateException in class EmailValidation

--- app/src/main/java/cc/somkiat/basicunittesting/validator/EmailValidator.java
+++ app/src/main/java/cc/somkiat/basicunittesting/validator/EmailValidator.java
@@ -2,13 +2,32 @@ package cc.somkiat.basicunittesting.validator;
 
 import java.util.regex.Pattern;
 
+import cc.somkiat.basicunittesting.exception.ValidateException;
+
 public class EmailValidator {
 
-    public boolean isEmailEmpty(String email) {
-        return email.isEmpty();
+    public ValidateResult validate(String email) {
+        try {
+            isNotEmailEmpty(email);
+            isEmailPatternCorrect(email);
+        } catch (ValidateException e) {
+            return new ValidateResult(false, e.getMessage());
+        }
+
+        return new ValidateResult(true, "Email is valid");
     }
 
-    public boolean isEmailPatternCorrect(String email) {
-        return Pattern.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}", email);
+    private void isNotEmailEmpty(String email) throws ValidateException {
+        if (email.isEmpty()) {
+            throw new ValidateException("Email is empty");
+        }
+    }
+
+    private void isEmailPatternCorrect(String email) throws ValidateException {
+        String emailPattern = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+
+        if (!Pattern.matches(emailPattern, email)) {
+            throw new ValidateException("Email Pattern is incorrect");
+        }
     }
 }
