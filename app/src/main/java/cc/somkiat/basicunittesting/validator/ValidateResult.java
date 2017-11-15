2d745755bb0d0e3cb425aabe234f5d536b7f8841
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-15 14:38:24 +0700

    created class ValidateResult

---
+++ app/src/main/java/cc/somkiat/basicunittesting/validator/ValidateResult.java
@@ -0,0 +1,27 @@
+package cc.somkiat.basicunittesting.validator;
+
+public class ValidateResult {
+    private boolean isValid;
+    private String message;
+
+    public ValidateResult(boolean isValid, String message) {
+        this.isValid = isValid;
+        this.message = message;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
